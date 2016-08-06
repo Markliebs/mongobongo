13709367a86e2751eb34c8f0240827829e889bc6
markliebs <markliebs@me.com>
2016-08-05 20:40:39 -0400

    Removed old stuff and trying to make stuff work

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,19 +1,19 @@
-$(document).ready(function(){
-	$("button").on('click', "#submit", function(){
+$(document).ready(function () {
+	$("button").on('click', "#submit", function () {
 		//console.log($(this).val());
 		$.ajax({
 		    type: "GET",
 		    url: '/submit',
 		    dataType: 'JSON',
 		  })
-		  .done(function(data) {
-		    console.log(data);
-		    //can build out wells here in a for loop or use handlebars helper function
-		  });
+			.done(function (data) {
+				console.log(data);
+				//can build out wells here in a for loop or use handlebars helper function
+			});
 		return false;
 	});
 
-	$("button").on('click', "#addComment", function(event){
+	$("button").on('click', "#addComment", function (event) {
 		//console.log($(this).val());
 		event.preventDefault();
 		$.ajax({
@@ -27,16 +27,16 @@ $(document).ready(function(){
 		      created: Date.now()
 		    }
 		  })
-		  .done(function(data) {
-		    console.log(data);
-		    $('#user').val("");
-		    $('#comment').val("");
-		    $('.comments').append("<p>"+data.user+": "+data.comment+"</p>");
-		  });
+			.done(function (data) {
+				console.log(data);
+				$('#user').val("");
+				$('#comment').val("");
+				$('.comments').append("<p>" + data.user + ": " + data.comment + "</p>");
+			});
 		return false;
 	});
 
-	$("button").on('click', "#delete", function(){
+	$("button").on('click', "#delete", function () {
 		$.ajax({
 		    type: "POST",
 		    url: '/delete',
@@ -48,9 +48,9 @@ $(document).ready(function(){
 		      created: $(this).val(),
 		    }
 		  })
-		  .done(function(response) {
-		    console.log(response);
-		  });
+			.done(function (response) {
+				console.log(response);
+			});
 		return false;
 	});
 })
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + '/public'));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 //Sets up calls for mongojs for local use
@@ -31,16 +31,16 @@ app.set('view engine', 'handlebars');
 var config = require('./config.js');
 var db = mongojs(config.dbURI, [config.collections])
 
-app.get('/', function(req, res) {
-	db.articles.find({}, function(err, data) {
+app.get('/', function (req, res) {
+	db.articles.find({}, function (err, data) {
 		if (err) throw err;
-		res.render("index", {articles: data});
+		res.render("index", { articles: data });
 	})
 })
 
-app.get('/submit', function(req, res) {
+app.get('/submit', function (req, res) {
 	//Loads the web page into the request package to grab information for the user to view
-	request("http://www.massivelyop.net", function(err, response, html) {
+	request("http://www.massivelyop.net", function (err, response, html) {
 
 		//Allows the use of $ similar to jQuery
 		var $ = cheerio.load(html);
@@ -49,10 +49,10 @@ app.get('/submit', function(req, res) {
 		var result = [];
 
 		//Finds each called element within the document and saves the pertinent ones for our page
-		$("article").each(function(i, element) {
+		$("article").each(function (i, element) {
 			var article = {};
 			//Grabs the titles of each article
-			var title = $(this).find("h2").children().text().trim();
+			var title = $(this).find("post-title").children().text().trim();
 
 			//Grabs the description of each article, if available
 			var desc = $(this).find("div.desc").text().trim();
@@ -75,12 +75,12 @@ app.get('/submit', function(req, res) {
 				console.log(article);
 				result.push(article);
 
-				db.articles.find({'title': article.title}, function(err, articleData) {
+				db.articles.find({ 'title': article.title }, function (err, articleData) {
 					if (articleData == true) {
 						console.log("Article already present.")
 					} else {
 						//inserts articles into db.
-						db.articles.save(article, function(err, data) {
+						db.articles.save(article, function (err, data) {
 							if (err) throw err;
 							//console.log(data);
 						});
@@ -94,29 +94,29 @@ app.get('/submit', function(req, res) {
 	res.send(result);
 });
 
-app.post('/addComment', function(req, res) {
+app.post('/addComment', function (req, res) {
 	//console.log(req.body);
 	var newComment = {
-			user: req.body.user,
-			comment: req.body.comment,
-			created: req.body.created,
-		};
-	db.articles.update({'_id': mongojs.ObjectId(req.body.id)}, {$push: {"comments": newComment}}, function(err, data) {
+		user: req.body.user,
+		comment: req.body.comment,
+		created: req.body.created,
+	};
+	db.articles.update({ '_id': mongojs.ObjectId(req.body.id) }, { $push: { "comments": newComment } }, function (err, data) {
 		if (err) throw err;
 		console.log(data);
 	});
 	res.send(newComment);
 });
 
-app.post('/delete', function(req, res) {
+app.post('/delete', function (req, res) {
 	var toBeDeleted = {
-			id: req.body.id,
-			user: req.body.user,
-			comment: req.body.comment,
-			created: req.body.created,
-		};
-		console.log(toBeDeleted);
-	db.articles.update({'_id': mongojs.ObjectId(toBeDeleted.id)}, {$pop: {'comments': 1}}, function(err, data) {
+		id: req.body.id,
+		user: req.body.user,
+		comment: req.body.comment,
+		created: req.body.created,
+	};
+	console.log(toBeDeleted);
+	db.articles.update({ '_id': mongojs.ObjectId(toBeDeleted.id) }, { $pop: { 'comments': 1 } }, function (err, data) {
 		if (err) throw err;
 		res.send(data);
 	});
@@ -124,6 +124,6 @@ app.post('/delete', function(req, res) {
 
 var port = process.env.PORT || 4200;
 // listen on port 4200
-app.listen(port, function() {
-  console.log('App running on port 4200!');
+app.listen(port, function () {
+	console.log('App running on port 4200!');
 });
